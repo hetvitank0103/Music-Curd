f9020d08bb2626404fa7dc6bb43fa003224a2375
hetvitanknm <hetvitanknm@gmail.com>
2026-08-02 19:14:42 +0530

    tracks/models.py done!

--- tracks/models.py
+++ tracks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    add_date = models.DateField(auto_now_add=True)
+    audio_file = models.FileField(upload_to='music_files/')
+    
+    def __str__(self):
+        return self.title
